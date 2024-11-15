0b8159dd793ef41514c7d13b8d5ade91c7cd3e1b
Daniel Lam <daniel.lam@pluto.tv>
2024-11-15 02:07:27 -0800

    Typescript coding.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0/');
+        if(!response.ok){
+          throw new Error("Failed to fetch Pokemon List.")
+        }
+        const jsonData = await response.json();
+        const results = jsonData.results;
+
+        const pokemonDetails = results.map(async(pokemon) => {
+          try {
+            const pokemonResponse = await fetch(pokemon.url);
+            if(!pokemonResponse.ok) throw new Error(`Failed to fetch ${pokemon.name}`);
+            const pokemonStats = await pokemonResponse.json();
+            return pokemonStats; 
+          } catch(error) {
+            console.log(error);
+            return null;
+          }
+        })
+        const stats = await Promise.all(pokemonDetails);
+        const sortedData = stats.filter(pokemon => pokemon !== null).sort((a,b) => b.weight - a.weight);
+        setData(sortedData);
+      } catch (error) {
+        setError(error);
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if(loading){
+    return <div>Loading...</div>
+  }
+  if(error){
+    return <div>{error}</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>API Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name} - {item.weight}</li>
+        ))}
+      </ul>
     </div>
   );
 }
